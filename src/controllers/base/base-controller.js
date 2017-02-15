a7738691693080e1bf44a5d69c3dfa0d2544467a
Sakthivel Manikam Arunachalam <sarunach@akamai.com>
2017-02-15 09:43:08 -0500

    Branch2 demo

--- src/controllers/base/base-controller.js
+++ src/controllers/base/base-controller.js
@@ -1,19 +1,55 @@
 (function (angular) {
 	'use strict';
 
-	function BaseController($scope, $rootScope) {
-		console.log("Base controller says Hello too!!!");
-		// console.log("Lets have a look at the $scope object below....");
-		// console.log($scope);
-		// console.log("Lets have a look at the $rootScope object below....");
-		// console.log($rootScope);
+	function BaseController($scope) {
+		// $scope variables
+		$scope.VAR1 = "I am the value of $scope.VAR1";
+		$scope.VAR2 = "I am the value of $scope.VAR2";
+
+		$scope.currentTime = (new Date()).getTime();
+
+		$scope.updateTime = function () {
+			// console.log($scope.$apply);
+			$scope.currentTime = (new Date()).getTime();
+			
+		};
+
+		// Update time after approx 8 seconds
+		$scope.autoUpdate = function () {
+			setTimeout(function() {
+	          $scope.currentTime = (new Date()).getTime();;
+	          console.log('Updated $scope Time: '+ $scope.currentTime);
+	        }, 8000);
+		};
+		$scope.autoUpdate();
+
+
+		var ctrl = this;
+		// Ctrl variables
+		ctrl.VAR1 = "I am the value of BaseController.VAR1";
+		ctrl.VAR2 = "I am the value of BaseController.VAR2";
+
+		ctrl.currentTime = (new Date()).getTime();
+
+		ctrl.updateTime = function () {
+			ctrl.currentTime = (new Date()).getTime();
+		};
+
+		// Update time after approx 4 seconds
+		ctrl.refreshScope = function () {
+			setTimeout(function () {
+				$scope.$apply(function() {
+					ctrl.currentTime = (new Date()).getTime();
+					console.log('Updated Controller Time: '+ $scope.currentTime);
+				});
+			}, 3000);
+		};
+		ctrl.refreshScope();
 	}
 
-	BaseController.$inject = ['$scope', '$rootScope'];
+	BaseController.$inject = ['$scope'];
 
-	function config() {
-		console.log("Base module config function says Hello!!!");
-	}
+	function config() {}
 
 	config.$inject = [];
 
